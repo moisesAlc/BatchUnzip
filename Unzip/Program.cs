d5f2cbf3ad29cf3a018cc8413e33be91537213df
Moisés Madeira <moisesprepara@gmail.com>
2022-06-10 21:35:09 -0300

    Adicionar arquivos de projeto.

---
+++ Unzip/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO.Compression;
+
+static void Main(string[] args)
+{
+    var diretorioInicial = Directory.GetCurrentDirectory();
+    var diretorios = Directory.EnumerateDirectories(diretorioInicial);
+
+    foreach (var item in diretorios)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+//List<string> retornaSubdiretorios(string
+
+void ExtraiEExcluiZip(string nomeArquivo)
+{
+    try
+    {
+        ZipFile.ExtractToDirectory(nomeArquivo, Directory.GetCurrentDirectory());
+        Console.WriteLine("Extracted {0}...", nomeArquivo);
+        File.Delete(nomeArquivo);
+        Console.WriteLine("Deleted {0}...", nomeArquivo);
+    }
+    catch (IOException e)
+    {
+        Console.Error.WriteLine(
+            "An error occurred while trying to delete {0}: {1}",
+            nomeArquivo, e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.Error.WriteLine(
+            "An error occurred while trying to extract {0}: {1}",
+            nomeArquivo, e.Message);
+    }
+}
+
+
+
+
+
+
+
